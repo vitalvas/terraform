6816defba5f88a8b64bbf79310d74df9e7292e1a
Vitaliy Vasylenko <me@vitalvas.com>
2024-05-27 20:16:35 -0400

    feat(aws-route53-dnssec-key): init

---
+++ src/aws-route53-dnssec-key/main.tf
@@ -0,0 +1,58 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "main" {
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 14
+  key_usage                = "SIGN_VERIFY"
+  description              = "KMS key for DNSSEC"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service",
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:route53:::hostedzone/*"
+          }
+        }
+      },
+      {
+        Action = "kms:CreateGrant",
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
+        Resource = "*"
+        Condition = {
+          Bool = {
+            "kms:GrantIsForAWSResource" = "true"
+          }
+        }
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+  })
+}
