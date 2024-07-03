b0a712bcc0983f3fde40437884623605b77a7b6e
Vitaliy Vasylenko <me@vitalvas.com>
2024-07-03 22:05:41 +0300

    feat: add catalog_data to aws-ecr-public

--- src/aws-ecr-public/main.tf
+++ src/aws-ecr-public/main.tf
@@ -1,3 +1,12 @@
 resource "aws_ecrpublic_repository" "main" {
   repository_name = var.name
+
+  catalog_data {
+    about_text        = var.about_text
+    architectures     = var.architectures
+    description       = var.description
+    logo_image_blob   = var.logo_image_blob
+    operating_systems = var.operating_systems
+    usage_text        = var.usage_text
+  }
 }
--- src/aws-ecr-public/variables.tf
+++ src/aws-ecr-public/variables.tf
@@ -1,3 +1,33 @@
 variable "name" {
   type = string
 }
+
+variable "about_text" {
+  type    = string
+  default = null
+}
+
+variable "architectures" {
+  type    = list(string)
+  default = []
+}
+
+variable "description" {
+  type    = string
+  default = null
+}
+
+variable "logo_image_blob" {
+  type    = string
+  default = null
+}
+
+variable "operating_systems" {
+  type    = list(string)
+  default = ["Linux"]
+}
+
+variable "usage_text" {
+  type    = string
+  default = null
+}
